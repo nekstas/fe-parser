5dd8b75a155336c466da6920c2368a44928db902
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-01-30 18:43:12 +0200

    Add common files for project.

---
+++ app/application.h
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+class Application {
+public:
+    Application() = default;
+
+    int32_t Run() {
+        return 0;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "app/application.h"
+
+class Tokenizer;
+
+class Parser;
+
+class Formatter;
+
+int main() {
+    Application app;
+    return app.Run();
+}
