00914da591e2e45a454af43a099f0aa740c4c3c6
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-01-31 21:06:27 +0200

    Change order of tokenizer parsers (don't care)

--- core/tokenizer/tokenizer_factory.cpp
+++ core/tokenizer/tokenizer_factory.cpp
@@ -5,11 +5,11 @@
 
 Tokenizer TokenizerCreator::Create(std::istream& input) const {
     Tokenizer tokenizer = Tokenizer(input);
+    tokenizer.AddParser<IntegerParser>();
     tokenizer.AddParser<OperatorParser<OperatorType::PLUS>>();
     tokenizer.AddParser<OperatorParser<OperatorType::MINUS>>();
     tokenizer.AddParser<OperatorParser<OperatorType::MULTIPLY>>();
     tokenizer.AddParser<OperatorParser<OperatorType::DIVIDE>>();
     tokenizer.AddParser<OperatorParser<OperatorType::POWER>>();
-    tokenizer.AddParser<IntegerParser>();
     return tokenizer;
 }
