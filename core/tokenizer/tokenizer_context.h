e419f2a5816b5c0436b8151252f4e6395d5f9fb5
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-01-31 19:44:26 +0200

    Add some abstractions

---
+++ core/tokenizer/tokenizer_context.h
@@ -0,0 +1,5 @@
+#pragma once
+
+struct TokenizerContext {
+    bool is_indent;
+};
--- core/tokenizer/tokens/abstract_token.h
+++ core/tokenizer/tokens/abstract_token.h
@@ -7,4 +7,17 @@ public:
     }
 
     virtual TokenType GetType() const = 0;
+
+    virtual std::string ToString() const = 0;
 };
+
+using Token = std::shared_ptr<AbstractToken>;
+
+template <typename T, typename... Args>
+Token MakeToken(Args&&... args) {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Token& token) {
+    return out << token->ToString();
+}
