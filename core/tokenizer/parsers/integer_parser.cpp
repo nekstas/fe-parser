c4379e114a158c56d1eddd8be22447c0d7822a08
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-01-31 21:04:12 +0200

    Remove useless code

--- core/tokenizer/parsers/integer_parser.cpp
+++ core/tokenizer/parsers/integer_parser.cpp
@@ -3,25 +3,16 @@
 #include "../tokens/integer_token.h"
 
 std::optional<Token> IntegerParser::TryParse(IStream& stream) const {
-    if (!IsNumberStart(stream)) {
-        return std::nullopt;
-    }
-
     std::string number_str;
     number_str.push_back(stream.Get());
     while (IsNumberSymbol(stream.Peek())) {
         number_str.push_back(stream.Get());
     }
 
+    if (number_str.empty()) {
+        return std::nullopt;
+    }
     return MakeToken<IntegerToken>(number_str);
-}
-
-bool IntegerParser::IsSignSymbol(char c) const {
-    return Contains(kSignSymbols, c);
-}
-
-bool IntegerParser::IsNumberStart(IStream& in) const {
-    return IsNumberSymbol(in.Peek()) || (IsSignSymbol(in.Peek()) && IsNumberSymbol(in.Peek(1)));
 }
 
 bool IntegerParser::IsNumberSymbol(int c) const {
--- core/tokenizer/parsers/integer_parser.h
+++ core/tokenizer/parsers/integer_parser.h
@@ -4,15 +4,10 @@
 #include "non_empty_parser.h"
 
 class IntegerParser : public NonEmptyParser {
-    static constexpr std::string_view kSignSymbols = "+-";
-
+    // TODO: Add collapsing of (+ or -) and number later.
 public:
     virtual std::optional<Token> TryParse(IStream& stream) const override;
 
 private:
-    bool IsSignSymbol(char c) const;
-
-    bool IsNumberStart(IStream& in) const;
-
     bool IsNumberSymbol(int c) const;
 };
