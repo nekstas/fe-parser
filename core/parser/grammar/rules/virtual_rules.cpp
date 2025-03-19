df9193aba5af839101afdc5bcee19386445da8cc
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-03-19 20:55:58 +0300

    Add more virtual rules

--- core/parser/grammar/rules/virtual_rules.cpp
+++ core/parser/grammar/rules/virtual_rules.cpp
@@ -1,16 +1,23 @@
 #include "virtual_rules.h"
 
+#include "../../../tokenizer/tokens/close_bracket_token.h"
 #include "../../../tokenizer/tokens/name_token.h"
+#include "../../../tokenizer/tokens/new_line_token.h"
+#include "../../../tokenizer/tokens/open_bracket_token.h"
 #include "../../../tokenizer/tokens/operator_token.h"
+#include "named_rule.h"
 #include "repeat_rule.h"
 #include "sequence_rule.h"
+#include "token_type_rule.hpp"
 #include "token_value_rule.hpp"
+#include "variant_rule.h"
 
 grammar_rules::GrammarRule grammar_rules::VirtualRules::SeparatorRule(
-    grammar_rules::GrammarRule element, grammar_rules::GrammarRule separator, bool accept_empty) {
+    grammar_rules::GrammarRule element, grammar_rules::GrammarRule separator, bool accept_empty
+) {
     return MakeRule<SequenceRule>(
-        {element,
-         MakeRule<RepeatRule>(MakeRule<SequenceRule>({separator, element}), accept_empty)});
+        {element, MakeRule<RepeatRule>(MakeRule<SequenceRule>({separator, element}), accept_empty)}
+    );
 }
 
 grammar_rules::GrammarRule grammar_rules::VirtualRules::KeywordRule(const std::string& name) {
@@ -19,4 +26,25 @@ grammar_rules::GrammarRule grammar_rules::VirtualRules::KeywordRule(const std::s
 
 grammar_rules::GrammarRule grammar_rules::VirtualRules::OperatorRule(const std::string& code) {
     return MakeRule<TokenValueRule<OperatorToken>>(code);
+}
+grammar_rules::GrammarRule grammar_rules::VirtualRules::Choice(
+    std::initializer_list<std::string> names
+) {
+    std::vector<GrammarRule> rules;
+    for (const std::string& name : names) {
+        rules.push_back(MakeRule<NamedRule>(name));
+    }
+    return MakeRule<VariantRule>(rules);
+}
+
+grammar_rules::GrammarRule grammar_rules::VirtualRules::OpenBracket() {
+    return MakeRule<TokenTypeRule<OpenBracketToken>>();
+}
+
+grammar_rules::GrammarRule grammar_rules::VirtualRules::CloseBracket() {
+    return MakeRule<TokenTypeRule<CloseBracketToken>>();
+}
+
+grammar_rules::GrammarRule grammar_rules::VirtualRules::NewLine() {
+    return MakeRule<TokenTypeRule<NewLineToken>>();
 }
--- core/parser/grammar/rules/virtual_rules.h
+++ core/parser/grammar/rules/virtual_rules.h
@@ -6,12 +6,21 @@ namespace grammar_rules {
 
 class VirtualRules {
 public:
-    static GrammarRule SeparatorRule(GrammarRule element, GrammarRule separator,
-                                     bool accept_empty = false);
+    static GrammarRule SeparatorRule(
+        GrammarRule element, GrammarRule separator, bool accept_empty = false
+    );
 
     static GrammarRule KeywordRule(const std::string& name);
 
     static GrammarRule OperatorRule(const std::string& code);
+
+    static GrammarRule Choice(std::initializer_list<std::string> names);
+
+    static GrammarRule OpenBracket();
+
+    static GrammarRule CloseBracket();
+
+    static GrammarRule NewLine();
 };
 
 }  // namespace grammar_rules
