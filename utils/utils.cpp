1252ee83bdad307618a6b720503c233e8589315e
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-03-20 22:56:57 +0300

    Add utils::Join function

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -3,11 +3,22 @@
 #include <fstream>
 #include <sstream>
 
-bool Contains(std::string_view str, char c) {
+bool utils::Contains(std::string_view str, char c) {
     return str.find(c) != std::string_view::npos;
 }
 
-std::string ReadFile(const std::string& filename) {
+std::string utils::Join(const std::vector<std::string>& strings, const std::string& delimiter) {
+    if (strings.empty()) {
+        return "";
+    }
+    std::string result = strings.front();
+    for (size_t i = 1; i < strings.size(); ++i) {
+        result += delimiter + strings[i];
+    }
+    return result;
+}
+
+std::string utils::ReadFile(const std::string& filename) {
     std::ifstream file(filename);
     std::stringstream buffer;
     buffer << file.rdbuf();
--- utils/utils.h
+++ utils/utils.h
@@ -1,8 +1,15 @@
 #pragma once
 
-#include <string_view>
 #include <string>
+#include <string_view>
+#include <vector>
+
+namespace utils {
 
 bool Contains(std::string_view str, char c);
 
+std::string Join(const std::vector<std::string>& strings, const std::string& delimiter);
+
 std::string ReadFile(const std::string& filename);
+
+}  // namespace utils
